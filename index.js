ed8d60e51386329676784094d0edecd582e4fc77
Jainish Prathmesh Shah <24f1002326@ds.study.iitm.ac.in>
2025-10-17 22:19:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -557,9 +557,10 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
 
 
